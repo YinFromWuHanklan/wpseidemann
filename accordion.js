060c19d9c281743746423f946f28acfb8fd5f6f6
Yin Yin Wu-Hanke <Y.Wu-Hanke@ellusion.de>
2021-12-11 01:06:00 +0100

    accordion styling

--- accordion.js
+++ accordion.js
@@ -1,10 +1,10 @@
 
-    var acc = document.getElementsByClassName("accordion");
+    var acc = document.getElementsByClassName("accordion-service");
     var i;
     
     for (i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
+        this.classList.toggle("active-service");
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
